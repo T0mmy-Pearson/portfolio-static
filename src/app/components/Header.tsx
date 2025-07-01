d95fa9eb2baa5f64cea4ebe1ede5f5ac299a5510
Tommy Pearson <tundraperson@gmail.com>
2025-07-01 09:52:46 +0100

    updated bio

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,14 +6,14 @@ export default function Header() {
       <h1 className='text-4xl font-bold mb-2'>Tommy Pearson</h1>
       <h2 className='text-2xl font-semibold mb-4 border-b border-[#9e0060]-200'>Full Stack Developer</h2>
       <p className='text-[#9e0060]'>
-        The variety and breadth of my work so far has led me to become ever more interested in developing the
-        technical skills required to work across industries. That&apos;s why I&apos;ve chosen to undertake a bootcamp with
-        Northcoders, to develop and grow my technical skills, to grow my work as a writer, artist and designer. These
-        experiences have enabled me to learn and cultivate a diverse range of skills, honed in roles within the arts,
-publishing and marketing. I bring a dynamism and energy to any role that tasks me to collaborate with
-others, think critically, prioritise and manage changing, tight deadlines. I am an adaptable, intuitive and
-passionate person with a diverse range of interests and my natural curiosity, innovativity, and analytical skill
-set means that I thrive in dynamic roles that demand learning and growth.</p>
+        With a diverse background spanning sound design, artistic direction, digital strategy, and creative facilitation, 
+        I bring a unique multidisciplinary perspective to full-stack development. From conceptualising immersive 
+        audio-visual performances to spearheading comprehensive web presence overhauls for energy and hospitality 
+        clients, my experience demonstrates adaptability and technical innovation across creative and digital domains. 
+        That&apos;s why I&apos;ve chosen to undertake a bootcamp with Northcoders - to synthesise my creative expertise with 
+        robust technical skills and continue pushing boundaries in digital innovation. I am an adaptable, intuitive and 
+        passionate person whose natural curiosity, innovation, and analytical skill set means that I thrive in dynamic 
+        roles that demand learning and growth.</p>
     </div>
   )
 }
